831698801322625a1e48d7a177278ddef92248cc
brvltd <bravie068@gmail.com>
2020-11-19 17:04:43 +0300

    changes effective

--- Js/loadData.js
+++ Js/loadData.js
@@ -19,6 +19,17 @@ function getData(){
 		}
 		seventh_image.innerHTML+=("<iframe style='margin-top:4%; transition: 5s ease;' src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3988.812277221013!2d36.789474615340396!3d-1.2867035359909806!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x182f10afb600df91%3A0xd4a9f8bfcf2fa234!2sMaalim%20Juma%20Rd%2C%20Nairobi!5e0!3m2!1sen!2ske!4v1605687393375!5m2!1sen!2ske' width='920' height='900' frameborder='0' style='border:0;' allowfullscreen='' aria-hidden='false' tabindex='0'></iframe>");
 
+		var database = firebase.database();
+		let ref=firebase.database().ref("Cargo/"+track_no);
+		ref.once('value').then(function (snapshot) {
+			var cargoName=snapshot.val().CargoName;
+			var custName=snapshot.val().CustomerName;
+			var trackN=snapshot.val().TrackNo;
+			console.log(cargoName,custName,trackN);
+			document.getElementById("id").innerHTML=trackN;
+			document.getElementById("name").innerHTML=custName;
+			document.getElementById("cargo").innerHTML=cargoName;
+		});
 		// sixth_image.innerHTML+=("<div class='col-md-6 fifth_image'></div>")
 
 		// var rootRef=firebase.database().ref().child("Cargo");
